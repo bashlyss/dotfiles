c4cb3bc76e63dc58407cf5ba1ee56a4f99e36f9d
james.loewen <james.loewen@zanbato.com>
2016-03-11 15:54:54 -0800

    First commit

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+FORCE=""
+if [[ $1 == "-f" ]]; then
+    FORCE=-f
+fi
+
+symlink() {
+    ln -s $FORCE $@
+}
+export symlink
+
+link_home() {
+    if [[ ! -L $HOME/.$1 || -n $FORCE ]]; then
+        symlink $PWD/$1 $HOME/.$1
+    fi
+}
+
+link_home vim
+link_home bashrc
+link_home ackrc
+link_home profile
+
+apt-get install < ubuntu_packages.txt
+
+if [[ $- == *i* ]]; then
+    source ~/.bashrc
+fi
